ee7344ef6a44c1b2a0dac40d7eb456f2e7b498cb
joysdestiny <joy.destinyshowers@gmail.com>
2026-09-16 13:03:31 -0400

    Realized I was working in the wrong repo so I moved the changes to the correct repo.

--- proj2/merge.cpp
+++ proj2/merge.cpp
@@ -1,7 +1,6 @@
 // merge.cpp
 
 #include "volsort.h"
-
 #include <iostream>
 
 // Prototypes
@@ -16,7 +15,10 @@ Node *merge(Node *left, Node *right, bool numeric);
 // sort algorithm. This function serves as a wrapper or helper function for the recursive msort function.
 void merge_sort(List &l, bool numeric) {
     // Handle base case
-
+if (l.head == nullptr || l.head->next == nullptr) {
+        return;
+    }
+    l.head = msort(l.head, numeric);
     // Divide into left and right sublists
 
     // Conquer left and right sublists
@@ -27,11 +29,37 @@ void merge_sort(List &l, bool numeric) {
 // msort is the recursive portion of the algorithm and calls split to divide and calls merge to conquer. It returns
 // the new head of the list.
 Node *msort(Node *head, bool numeric) {
+    Node *left = nullptr;
+    Node *right = nullptr;
+
+    if (head == nullptr || head->next == nullptr) {
+        return head;
+    } else {
+        split(head, left, right);
+        left = msort(left, numeric);
+        right = msort(right, numeric);
+        return merge(left, right, numeric);
+    }
 }
 
 // split is a helper function that splits the singly-linked list in half by using the slow- and fast- pointer technique
 // (aka [tortoise and hare]).
 void split(Node *head, Node *&left, Node *&right) {
+    if (head == nullptr || head->next == nullptr) {
+        left = head;
+        right = nullptr;
+        return;
+    }
+    Node *slow = head;
+    Node *fast = head->next;
+
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    right = slow->next;
+    left = head;
+    slow->next = nullptr; // break the list into two halves
 }
 
 // merge is a helper function that combines both the left and right lists and returns the new head of the list.
@@ -41,8 +69,46 @@ Node *merge(Node *left, Node *right, bool numeric) {
     Node* tail = &dummy;
 
     // combine both left and right lists
+    if (numeric) {
+        while (left != nullptr && right != nullptr) {
+            if (node_number_compare(left, right)) {
+                tail->next = left;
+                left = left->next;
+                tail = tail->next;
+                tail->next = nullptr;
+            } else {
+                tail->next = right;
+                right = right->next;
+                tail = tail->next;
+                tail->next = nullptr;
+            }
+        }
+        if (left != nullptr) {
+            tail->next = left;
+        } else if (right != nullptr) {
+            tail->next = right;
+        }
+        return dummy.next;
+    } else {
+        while (left != nullptr && right != nullptr) {
+            if (node_string_compare(left, right)) {     // compare the values of each node and then build a new one 
 
-    // compare the values of each node and then build a new one
-    
-    
+                tail->next = left;
+                left = left->next;
+                tail = tail->next;
+                tail->next = nullptr;
+            } else {
+                tail->next = right;
+                right = right->next;
+                tail = tail->next;
+                tail->next = nullptr;
+            }
+        }
+        if (left != nullptr) {
+            tail->next = left;
+        } else if (right != nullptr) {
+            tail->next = right;
+        }
+        return dummy.next;
+    }
 }
