1f91c55e25e3130619527148a727d8e4fdd1b098
pmyersCE <167543795+pmyersCE@users.noreply.github.com>
2026-09-16 15:24:25 -0400

    Update qsort.cpp
    Copied over code from STL sort cpp file, copied the provided comparison int function, and changed it to be a comparison string function. More comments TBA, but everything else should be working as of now. Used the example from Maggie's slides in lab, "sizeof(Node *)" etc.

--- proj2/qsort.cpp
+++ proj2/qsort.cpp
@@ -4,9 +4,66 @@
 
 #include <cstdlib>
 #include <array>
+#include <vector>
 #include <iostream>
 
-void qsort_sort(List &l, bool numeric) {
+using namespace std;
 
+int qComparisonInt(const void * a, const void * b) {
+
+       const Node * ia = *(const Node **) a;
+       const Node * ib = *(const Node **) b;
+
+       if (ia->number < ib->number) return -1;  // sorts in ascending order
+       if (ia->number > ib->number) return 1;
+       return 0;                                                                                         
+}
+
+int qComparisonStr(const void * a, const void * b) {
+
+       const Node * ia = *(const Node **) a;
+       const Node * ib = *(const Node **) b;
+
+       if (ia->string < ib->string) return -1;  // sorts in ascending order
+       if (ia->string > ib->string) return 1;
+       return 0;                                                                                         
+}
+
+void qsort_sort(List &l, bool numeric) {
+    
+    if (l.head == nullptr){
+        return;
+    }
+    
+    Node* current = l.head;
+    vector<Node *> nodes;
+
+    // #1 populate vector with sorted numbers
+    while (current->next != nullptr) {
+        nodes.push_back(current);
+        current = current->next;
+    }
+        nodes.push_back(current); // account for the last node.
+
+    // #2 https://man7.org/linux/man-pages/man3/qsort.3.html
+    if(numeric){
+        // qsort(&argv[1], argc - 1, sizeof(char *), cmpstringp);
+        qsort(&nodes[0], nodes.size(), sizeof(Node *), qComparisonInt);
+    } else {
+        qsort(&nodes[0], nodes.size(), sizeof(Node *), qComparisonStr);
+    }
+
+    // #3 where we walk the vector and fix the pointers
+    for (int i = 0; i < nodes.size(); ++i) {
+        if(i < nodes.size()-1){
+            nodes.at(i)->next = nodes.at(i+1);
+        } else {
+            nodes.at(i)->next = nullptr;
+        }
+    }
+
+    //reset the head of the list to the front of our sorted list of nodes
+    l.head = nodes[0];
+  
 }
 
